1d3a29d3021a533d13f3d7413eca0c223e4364e2
William Azevedo <claudiosva1@gmail.com>
2024-05-04 17:00:51 +0100

    componentes tela login e cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
