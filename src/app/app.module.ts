534da1126597172d34740f8de8d914048bb7780a
William Azevedo <claudiosva1@gmail.com>
2024-05-04 18:46:03 +0100

    Componente menu e rodape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    CadastroComponent
+    CadastroComponent,
+    MenuComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
@@ -22,3 +28,5 @@ import { CadastroComponent } from './components/cadastro/cadastro.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
