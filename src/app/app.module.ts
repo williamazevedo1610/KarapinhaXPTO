80bf7f106db30832a05f270d4297fe91b9449c43
William Azevedo <claudiosva1@gmail.com>
2024-05-04 17:02:40 +0100

    Correcção no app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
 
 @NgModule({
