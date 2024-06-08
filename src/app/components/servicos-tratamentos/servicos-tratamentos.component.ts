863d35e94fde5012a7f3fc96a80d1a7e86ad530f
williamazevedo1610 <claudiosva1@gmail.com>
2024-06-08 05:23:26 +0100

    mais coisas

--- src/app/components/servicos-tratamentos/servicos-tratamentos.component.ts
+++ src/app/components/servicos-tratamentos/servicos-tratamentos.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './servicos-tratamentos.component.css'
 })
 export class ServicosTratamentosComponent {
+  services = [
+    { id: 1, image: '../../../assets/gallery/pexels-gabby-k-6621253.jpg', name: 'Serviço 1', description: 'Tratamento Profundo e mais outras coisas blablablabla, teste' },
+    { id: 2, image: 'path-to-image2.jpg', name: 'Serviço 2', description: 'Descrição do serviço 2' },
+    { id: 3, image: 'path-to-image3.jpg', name: 'Serviço 3', description: 'Descrição do serviço 3' },
+    { id: 4, image: 'path-to-image4.jpg', name: 'Serviço 4', description: 'Descrição do serviço 4' },
+    { id: 5, image: 'path-to-image5.jpg', name: 'Serviço 5', description: 'Descrição do serviço 5' },
+    { id: 6, image: 'path-to-image6.jpg', name: 'Serviço 6', description: 'Descrição do serviço 6' },
+    // Adicione mais serviços conforme necessário
+  ];
 
 }
